a47c9d204b0e9959fa04b30a3977f3b9d8fd739d
Alan Luo <i@iltc.io>
2021-05-11 21:26:33 -0400

    Finish weather api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApiController extends Controller
 {
-    //
+    public function callAction($method, $parameters) {
+        return parent::callAction($method, $parameters);
+    }
+
+    public function weather(Request $request) {
+        $url = 'https://api.openweathermap.org/data/2.5/onecall?lat='.env('WEATHER_LAT').'&lon='.env('WEATHER_LON').'&units=imperial&appid='.env('OPENWEATHERMAP_APPID');
+        $response = Http::get($url);
+
+        if ($response->ok()) {
+            return response()->json([
+                'status' => 'success',
+                'current' => $response['current'],
+                'today' => $response['daily'][0],
+                'tomorrow' => $response['daily'][1]
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'fail',
+                'reason' => 'HTTP Request Returns '.$response->status()
+            ], $response->status());
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/status', function (Request $request) {
+    return response()->json(['status' => 'success']);
+});
+
+use App\Http\Controllers\ApiController;
+
+Route::get('/weather', [ApiController::class, 'weather']);
